0f2cda948eea13c0480daee7db4fa4e9820eb131
daniakutsev <daniakutsev1488@gmail.com>
2022-11-21 19:28:09 +0200

    14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,28 +8,6 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
-  inputValue = ''
+  title = 'Initial'
 
-  title = 'Dynamic title'
-  number = 42
-  arr = [1, 2, 3]
-  obj = {a: 1, b: {c: 2}}
-
-  // img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFc_jMja-dFi6-ABOdQ3GSxSQoL88lbwwOIA&usqp=CAU"
-
-  // @ts-ignore
-  constructor() {
-    // setTimeout(() => {
-    //   this.img = "https://cdn.iconscout.com/icon/free/png-256/angular-2752246-2285063.png"
-    // }, 3000)
-  }
-
-  onInput(event: any) {
-    this.inputValue = (<HTMLInputElement>event.target).value
-    console.log(event)
-  }
-
-  onBlur(str: string) {
-    this.inputValue = str
-  }
 }
