67429e3f225fe4c123a7205135226ab5ab0d4741
daniakutsev <daniakutsev1488@gmail.com>
2022-11-22 13:30:31 +0200

    19 ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,27 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
-  toggle:any = false
+  arr = [1, 1, 3, 5, 8, 13]
+
+  objs = [
+    {
+      title: "post1",
+      name: "Dania",
+      comments: [
+        {text: "comm1", name: "Max"},
+        {text: "comm1", name: "Max"},
+        {text: "comm1", name: "Max"}
+      ]
+    },
+    {
+      title: "post2",
+      name: "Dania2",
+      comments: [
+        {text: "comm2", name: "Max"},
+        {text: "comm2", name: "Max"},
+        {text: "comm2", name: "Max"}
+      ]
+    },
+  ]
 
 }
