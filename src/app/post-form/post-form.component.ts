c74e6b67250da80b4293db303882ca1e61d2deb9
daniakutsev <daniakutsev1488@gmail.com>
2022-11-22 18:20:28 +0200

    24 focus

--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import {Post} from "../app.component";
 
 
@@ -10,6 +10,9 @@ import {Post} from "../app.component";
 export class PostFormComponent implements OnInit {
 
   @Output() onAdd: EventEmitter<Post> = new EventEmitter<Post>()
+
+  // @ts-ignore
+  @ViewChild('titleInput') inputRef: ElementRef
 
   title = ''
   text = ''
@@ -31,6 +34,10 @@ export class PostFormComponent implements OnInit {
       this.title = this.text = ''
     }
 
+  }
+
+  focusTitle() {
+    this.inputRef.nativeElement.focus()
   }
 
 }
