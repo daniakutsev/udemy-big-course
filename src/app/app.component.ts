d9348b1c1d6c0475bfeed250cd73fd469c6436a9
daniakutsev <daniakutsev1488@gmail.com>
2022-11-22 14:08:39 +0200

    22 form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,27 +8,4 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
-  arr = [1, 1, 3, 5, 8, 13]
-
-  objs = [
-    {
-      title: "post1",
-      name: "Dania",
-      comments: [
-        {text: "comm1", name: "Max"},
-        {text: "comm1", name: "Max"},
-        {text: "comm1", name: "Max"}
-      ]
-    },
-    {
-      title: "post2",
-      name: "Dania2",
-      comments: [
-        {text: "comm2", name: "Max"},
-        {text: "comm2", name: "Max"},
-        {text: "comm2", name: "Max"}
-      ]
-    },
-  ]
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {PostComponent} from "./post/post.component";
-import {Post2Component} from "./post2/post2.component";
 import {FormsModule} from "@angular/forms";
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PostFormComponent,
     PostComponent,
-    Post2Component
   ],
   imports: [
     BrowserModule,
