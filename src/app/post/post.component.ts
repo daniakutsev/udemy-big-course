5204b19308605e6fb02fed981a21832aa30093a6
daniakutsev <daniakutsev1488@gmail.com>
2022-11-23 11:33:47 +0200

    29 a

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,17 +1,32 @@
-import {Component, ContentChild, ElementRef, Input, OnInit} from '@angular/core';
+import {
+  Component,
+  ContentChild,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewEncapsulation
+} from '@angular/core';
 import {Post} from "../app.component";
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
+  encapsulation:ViewEncapsulation.None
 })
 export class PostComponent implements OnInit {
 
   // @ts-ignore
   @Input() post: Post
+  @Output() onRemove = new EventEmitter<number>()
   // @ts-ignore
   @ContentChild('info', {static: true}) infoRef: ElementRef
+
+  removePost() {
+    this.onRemove.emit(this.post.id)
+  }
 
   constructor() {
   }
