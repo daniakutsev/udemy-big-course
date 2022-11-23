a1ff3773b7366bcfc6eb259e90ea415490e0fcd4
daniakutsev <daniakutsev1488@gmail.com>
2022-11-23 13:20:01 +0200

    32

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
@@ -7,8 +7,13 @@ export class StyleDirective {
 
   constructor(private el: ElementRef, private r: Renderer2) {
     // el.nativeElement.style.color = 'red'
-    r.setStyle(el.nativeElement, 'color', 'blue')
+    //r.setStyle(el.nativeElement, 'color', 'blue')
   }
 
-
+  @HostListener('mouseenter', ['$event']) onEnter() {
+    this.r.setStyle(this.el.nativeElement, 'color', 'blue')
+  }
+  @HostListener('mouseleave', ['$event']) onLeave() {
+    this.r.setStyle(this.el.nativeElement, 'color', 'red')
+  }
 }
