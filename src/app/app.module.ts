2d1185e7aa9a1acc657eb5632d1d8ee7d5361e3a
daniakutsev <daniakutsev1488@gmail.com>
2022-11-24 21:46:58 +0200

    37 my pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
+import {MultByPipe} from "./pipe/multBy.pipe";
 @NgModule({
   declarations: [
     AppComponent,
+    MultByPipe
   ],
   imports: [
     BrowserModule,
