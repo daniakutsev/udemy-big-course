c1f4c6bede7e661429b9b101a7efeeadc0a1324f
daniakutsev <daniakutsev1488@gmail.com>
2022-11-29 13:40:43 +0200

    63 my ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
-import {MyValidators} from "./my.validators";
-
 
 @Component({
   selector: 'app-root',
@@ -9,55 +6,9 @@ import {MyValidators} from "./my.validators";
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  appState: string = 'on';
 
-  // @ts-ignore
-  form: FormGroup
-
-  ngOnInit() {
-    // @ts-ignore
-    // @ts-ignore
-    // @ts-ignore
-    // @ts-ignore
-    // @ts-ignore
-    this.form = new FormGroup({
-      // @ts-ignore
-      email: new FormControl('', [
-          Validators.email,
-          Validators.required,
-          MyValidators.restrictedEmail
-        ], [MyValidators.uniqEmail]
-      ),
-      password: new FormControl('', [
-        Validators.required,
-        Validators.minLength(5)
-      ]),
-      address: new FormGroup({
-        country: new FormControl('ua'),
-        city: new FormControl('')
-      }),
-
-    })
+  ngOnInit(): void {
   }
-
-  submit() {
-    const formData = {...this.form.value}
-    console.log(formData)
-    this.form.reset()
-  }
-
-  chooseCapital() {
-    const cityMap = {
-      ua: 'Киев',
-      ru: 'Москва',
-      by: 'Минск'
-    }
-    // @ts-ignore
-    const cityKey = this.form?.get('address')?.get('country').value
-    // @ts-ignore
-    const city = cityMap[cityKey]
-
-    this.form.patchValue({address: {city}})
-  }
-
 
 }
