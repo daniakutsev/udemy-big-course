f3a03fca8c11d788bf9ee84a811da07d0161c7ff
daniakutsev <daniakutsev1488@gmail.com>
2022-11-29 14:49:27 +0200

    65 getting data and output to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+export interface Todo {
+  completed: boolean,
+  title: string,
+  id?: number
+}
 
 @Component({
   selector: 'app-root',
@@ -7,8 +14,16 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
+  todos: Todo[] = []
+
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit(): void {
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .subscribe(response => {
+        this.todos = response
+      })
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, } from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -11,7 +12,7 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
