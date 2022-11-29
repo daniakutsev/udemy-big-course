607e74aaa8df29a65bc0b61290fb610651a9a006
daniakutsev <daniakutsev1488@gmail.com>
2022-11-29 15:06:12 +0200

    66 create obj with post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,15 +15,46 @@ export interface Todo {
 export class AppComponent implements OnInit {
 
   todos: Todo[] = []
+  todoTitle: string = ''
 
   constructor(private http: HttpClient) {
   }
 
-  ngOnInit(): void {
-    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=5')
+  getTodoByLimit(): void {
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
       .subscribe(response => {
         this.todos = response
       })
   }
 
+  getTodoById(): void {
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos/201')
+      .subscribe(response => {
+        this.todos = response
+      })
+  }
+
+  ngOnInit(): void {
+    this.getTodoByLimit()
+  }
+
+
+  addTodo() {
+    if (!this.todoTitle.trim()) {
+      return
+    }
+
+    const newTodo: Todo = {
+      title: this.todoTitle,
+      completed: false
+    }
+
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+      .subscribe(response => {
+        console.log(response)
+        this.todos.push(response)
+        this.todoTitle=''
+      })
+
+  }
 }
