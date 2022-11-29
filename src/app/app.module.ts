854d7a45001e2ee5c13a15f03099194c38dd75db
daniakutsev <daniakutsev1488@gmail.com>
2022-11-29 13:54:06 +0200

    64 start http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { SwitchComponent } from './switch/switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SwitchComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
