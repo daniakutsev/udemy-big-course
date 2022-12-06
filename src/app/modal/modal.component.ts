e8c2029b31f8b32d311689d9208e308a97968ea4
daniakutsev <daniakutsev1488@gmail.com>
2022-12-06 13:09:40 +0200

    96 Dynamic Component part 1

---
+++ src/app/modal/modal.component.ts
@@ -0,0 +1,19 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent implements OnInit {
+
+  @Input() title: string = ''
+  @Output() close = new EventEmitter<void>()
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
