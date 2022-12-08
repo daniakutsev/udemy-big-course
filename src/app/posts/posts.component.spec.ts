6da21c7e5701dfec39da76a1f18986dac8c8c8c1
daniakutsev <daniakutsev1488@gmail.com>
2022-12-08 08:39:59 +0200

    106 test a service. part 3 method remove

--- src/app/posts/posts.component.spec.ts
+++ src/app/posts/posts.component.spec.ts
@@ -47,6 +47,21 @@ describe('PostsComponent', () => {
 
     expect(component.message).toBe(message)
   });
+  it('should remove post if user confirms', () => {
+    const spy = spyOn(service, 'remove').and.returnValue(EMPTY)
+    spyOn(window, 'confirm').and.returnValue(true)
+
+    component.delete(10)
+    expect(spy).toHaveBeenCalledWith(10)
+  })
+  it('should NOT remove post if user doesn`t confirms', () => {
+    const spy = spyOn(service, 'remove').and.returnValue(EMPTY)
+    spyOn(window, 'confirm').and.returnValue(false)
+
+    component.delete(10)
+    expect(spy).not.toHaveBeenCalled()
+  })
+
 })
 
 
