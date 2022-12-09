2fc8002fefb20412c6887468c79ab69c3a59364e
daniakutsev <daniakutsev1488@gmail.com>
2022-12-09 08:55:05 +0200

    113 router test start

--- src/app/routing/routing.component.spec.ts
+++ src/app/routing/routing.component.spec.ts
@@ -1,7 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RoutingComponent } from './routing.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {RoutingComponent} from './routing.component';
 import {RouterTestingModule} from "@angular/router/testing";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {Observable} from "rxjs";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+
+class RouterStub {
+  navigate(path: string[]) {
+  }
+}
+
+class ActivatedRouterStub {
+  // @ts-ignore
+  params: Observable<Params>
+}
 
 describe('RoutingComponent', () => {
   let component: RoutingComponent;
@@ -9,8 +21,12 @@ describe('RoutingComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ RoutingComponent ],
+      declarations: [RoutingComponent],
       imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [
+        {provide: Router, useClass: RouterStub},
+        {provide: ActivatedRoute, useClass: ActivatedRouterStub}
+      ]
     })
 
     fixture = TestBed.createComponent(RoutingComponent);
@@ -19,5 +35,14 @@ describe('RoutingComponent', () => {
 
   it('should be defined', () => {
     expect(component).toBeDefined();
+  });
+
+  it('should been a called navigation in f goBack()', () => {
+    let router = TestBed.get(Router)
+    let spy = spyOn(router, 'navigate')
+
+    component.goBack()
+    expect(spy).toHaveBeenCalledWith(['/posts'])
+
   });
 });
