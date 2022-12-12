0c7d387a8d12a5139ffe87f334d5128299eabeb0
daniakutsev <daniakutsev1488@gmail.com>
2022-12-12 18:58:51 +0200

    121 definition of states

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {state, style, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('box', [
+      state('start', style({background: 'blue'})),
+      state('end', style({
+        background: 'red',
+        transform: 'scale(1.2)'
+      })),
+
+    ])
+  ]
 })
 export class AppComponent {
+  boxState = 'start'
 
+  animate() {
+    this.boxState = this.boxState === 'end' ? 'start' : 'end'
+  }
 }
